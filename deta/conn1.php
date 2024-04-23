b99cd04c6d5adf8db9973a4d9d2c35c5e3ff3015
prem-modi <84839567+prem-modi@users.noreply.github.com>
2024-04-23 11:00:48 +0530

    Update conn1.php

--- deta/conn1.php
+++ deta/conn1.php
@@ -1,18 +1,1 @@
 <?php
-
-$root = $_SERVER['DOCUMENT_ROOT'];
-$envFilepath = "$root/.env";
-$env = parse_ini_file($envFilepath);
-
-$servername = $env['servername'];
-$username = $env['username'];
-$password = '4563233ssssssssssss';
-$dbname = $env['dbname'];
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-?>
