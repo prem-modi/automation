aa61d8f8cb3444c6175709dca57f875f3129d59d
prem-modi <84839567+prem-modi@users.noreply.github.com>
2024-05-14 13:13:34 +0530

    Update conn.php

--- deta/conn.php
+++ deta/conn.php
@@ -1,1 +1,15 @@
 <?php
+
+// AWS EC2 MySQL DB Server
+$servername = "3.69.166.243";
+$username = "dns0108prd";
+$password = "VMS>*yUkhbo0Ot0-->";
+$dbname = "dns_app_prod";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
