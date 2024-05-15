553c58b0a37943b7b0cb3d1756eabe3695ef6c4f
prem-modi <84839567+prem-modi@users.noreply.github.com>
2024-05-15 18:22:41 +0530

    Update ngrokDemo.php

--- ngrokDemo.php
+++ ngrokDemo.php
@@ -1,4 +1,17 @@
 <?php
+// Command to be executed in Command Prompt
+$command = 'ngrok http 8090';
 
-echo "sss";
+// Execute the command in Command Prompt
+$command_two = 'start cmd php demo68.php';
+
+exec("start cmd /k \"{$command}\" 2>&1"."&& "."start cmd /k php demo68.php 2>&1");
+
+echo "Output: \n";
+foreach ($output as $line) {
+    echo $line . "\n";
+}
+
+// Print the return code
+echo "Return code: $returnCode\n";
 ?>
