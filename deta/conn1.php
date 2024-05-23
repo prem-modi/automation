dd0f5e23efe9f833bec861004849231a7fd8cfd1
prem-modi <84839567+prem-modi@users.noreply.github.com>
2024-05-23 11:17:17 +0530

    Update conn1.php

--- deta/conn1.php
+++ deta/conn1.php
@@ -1,7 +1,7 @@
 // <?php
 
 // // AWS EC2 MySQL DB Server
-// $servername = "3.69.166.243ssss";
+// $servername = "3.69.166.243ssssss";
 // $username = "dns0108prds";
 // $password = "VMS>*yUkhbo0Ot0-->ss";
 // $dbname = "dns_app_prosdsssss";
