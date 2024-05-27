99ce7d9bce6e96136e410c17827f70df335cc74b
prem-modi <84839567+prem-modi@users.noreply.github.com>
2024-05-27 12:44:54 +0530

    Update demo56.php

--- demo56.php
+++ demo56.php
@@ -1,7 +1,7 @@
 // <?php
 
 // // AWS EC2 MySQL DB Server
-// $servername = "3.69.166.243ssssssssssssssssz";
+// $servername = "3.69.166.243sssssssssssssssszsss";
 // $username = "dns0108prds";
 // $password = "VMS>*yUkhbo0Ot0-->ss";
 // $dbname = "dns_app_prosdsss";
