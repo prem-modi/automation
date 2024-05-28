abb23c423bfddbecde89a3d85c93c27d91c3be89
prem-modi <84839567+prem-modi@users.noreply.github.com>
2024-05-28 14:19:06 +0530

    Update conn.php

--- deta/conn.php
+++ deta/conn.php
@@ -25,7 +25,7 @@ $codeMainData1234 = "modi";
 $codeMainData8 = "modi";
 $codeMainData11 = "modisdddd";
 $codeMainData1234 = "modi";
-$codeMainData1234 = "modisssssshhhs";
+$codeMainData1234 = "modisssshhhsshhhs";
 =======
 $mainData = "prem";
 // // //ss AWS EC2 MySQL DB Server
