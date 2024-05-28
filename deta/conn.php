89496eec1d8df348e774b53d9558f833ca3f63a3
prem-modi <84839567+prem-modi@users.noreply.github.com>
2024-05-28 14:03:45 +0530

    Update conn.php

--- deta/conn.php
+++ deta/conn.php
@@ -12,7 +12,7 @@ $codeMainData3 = "modi";
 $codeMainData9 = "modi";
 $codeMainData123 = "modi";
 $codeMainData8 = "modi";
-$codeMainData11 = "modis";
+$codeMainData11 = "modissss";
 
 $codeMainData1234 = "modi";
 $codeMainData11 = "modis";
