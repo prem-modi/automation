1a492dc0ca084caac888edb24bd34fa5bbb5d517
prem-modi <84839567+prem-modi@users.noreply.github.com>
2025-09-19 13:42:16 +0530

    Update conn.php

--- deta/conn.php
+++ deta/conn.php
@@ -19,22 +19,12 @@ $names = "prem";
 $codes = "prem";
 $codeData = "modddddssssi";
 $codeData1 = "modi";
-$codeData = "modddddssssi";
+$password = "modddddssssi";
 $codeData1 = "modi";
 $ookcode = "modi";
-// // //ss AWS EC2 MySQL DB Server
 echo "ssssssseeee";
 
-// $servername = "3.69.166.243sssssssssssssssssssssggssssssss";
 $names2 = "modi";
 $names3 = "modi";
-// $username = "dns0108prds";
-// $password = "VMS>*yUkhbo0Ot0-->ss";
-// $dbname = "dns_app_prosdsss";
 
-// $conn = new mysqli($servername, $username, $password, $dbname);
-// // Check connection
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
 // ?>
